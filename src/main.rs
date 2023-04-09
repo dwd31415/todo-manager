c4d89df2e05e66eaf8f7905232f098dd5c2f9419
Adrian Dawid <adriandwd@gmail.com>
2023-04-10 00:23:50 +0200

    Add clap cli tool setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+/**
+Application to automatically format todo list written in code.
+*/
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path(s) to the files to be scanned for TODO-items.
+    #[clap(short, long, num_args = 0..)]
+    input_files: Vec<std::path::PathBuf>,
+
+    /// Name of the output file. 
+    /// Note: This file should already exist and contain the macro "@TODO-List" somewhere.
+    #[arg(short, long="output")]
+    output_file: std::path::PathBuf
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
